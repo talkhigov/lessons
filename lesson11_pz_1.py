49447dcbfc4b922910b0089ba193173ce9642910
Talkhigov <ilmantalkhigov@mail.ru>
2023-04-12 16:17:42 +0300

    lessons11_pz_1

--- lesson11_pz_1.py
+++ lesson11_pz_1.py
@@ -2,16 +2,16 @@ import os
 
 def create_folders():
     for i in range(1, 10):
-        folder_name = f'dir{i}'
+        folder_name = f'dir_{i}'
         os.mkdir(folder_name)
         print(f'папка {i} создана')
     
-# print(create_folders())
+# create_folders()
 
 def remove_folders():
     for i in range(1, 10):
-        folder_name = f'dir{i}'
+        folder_name = f'dir_{i}'
         os.rmdir(folder_name)
         print(f'папка {i} удалена')
 
-# print(remove_folders())
+# remove_folders()
